147e78b69c6dd98ce106c8642628ee5becc8f743
Your Name <you@example.com>
2025-08-08 14:25:02 +0000

    clear all the elements and codes did for previous next authentication

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    serverActions: true,
+    serverActions: {
+      bodySizeLimit: '4.5mb',
+    },
   },
   typescript: {
     ignoreBuildErrors: true,
